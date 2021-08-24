2d9208bb4317f87b9dc2a4cd1f95e3a500216717
mishkajudoshka <ulizkomikle@mail.ru>
2021-08-24 14:44:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import statistics
+
+def err(data):
+    mean = statistics.mean(data)
+    res = 0
+    for i in data:
+        res += abs(i-mean)
+    res = res / mean / len(data) * 100
+    return res
+
+if __name__ == '__main__':
+    values = [120, 95, 90, 85, 85]
+    p = [0.2 for i in range(5)]
+    std = 0
+    mean = statistics.mean(values)
+    for i in range(5):
+        std += values[i]**2*p[i]
+    std -= mean**2
+    std2 = statistics.stdev(values)
+    print(mean, std**0.5, err(values))
+
